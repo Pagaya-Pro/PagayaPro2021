02436c8151dd61e5567be1d947eab59b91a6932f
shais <shai.shechter@pagaya.com>
2021-08-24 13:01:21 +0300

    pagayapro utils

---
+++ pagayapro/data_paths/data_paths.py
@@ -0,0 +1,12 @@
+import os
+
+DATA_FILES_PATH = "s3://pagaya-pro-bucket/data"
+
+ASSIGNMENT1_DATA = os.path.join(DATA_FILES_PATH, "assignment1")
+ASSIGNMENT2_DATA = os.path.join(DATA_FILES_PATH, "assignment2")
+ASSIGNMENT3_DATA = os.path.join(DATA_FILES_PATH, "assignment3")
+ASSIGNMENT4_DATA = os.path.join(DATA_FILES_PATH, "assignment4")
+ASSIGNMENT5_DATA = os.path.join(DATA_FILES_PATH, "assignment5")
+ASSIGNMENT6_DATA = os.path.join(DATA_FILES_PATH, "assignment6")
+ASSIGNMENT7_DATA = os.path.join(DATA_FILES_PATH, "assignment7")
+ASSIGNMENT8_DATA = os.path.join(DATA_FILES_PATH, "assignment8")
