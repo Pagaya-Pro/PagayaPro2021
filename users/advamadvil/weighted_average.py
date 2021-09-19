ccf23fc1319211761294cad602bb1598dcbaeab9
Adva Madvil <advamadvil@advas-macbook-pro.local>
2021-09-19 17:47:17 +0300

    fix the code

---
+++ users/advamadvil/weighted_average.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+def compute_weighted_average(x: List[float],
+                             w: List[float])-> float:
+    return sum([x1 * w1 for x1, w1 in zip(x, w)]) / sum(w) if sum(w) != 0 else 0
+
+def check_compute_weighted_average(x:List[float],
+                                   w:List[float])-> float:
+    ans = compute_weighted_average(x, w)
+    print(f"The weighted average of {x} with weights {w} is {ans}")
+    return ans
+
+def main():
+    check_compute_weighted_average([1,2,3],[4,5,6])
+    check_compute_weighted_average([1, 0 ,1], [1, -1, 0])
+    check_compute_weighted_average([5,5,5], [0, 2, 4])
+
+if __name__=="__main__":
+    main()
