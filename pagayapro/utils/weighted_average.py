720b418655807f87e1b4cf57cdef4b6c8df5c7a5
shais <shai.shechter@pagaya.com>
2021-09-22 18:12:12 +0300

    undid change to weighted_average

--- pagayapro/utils/weighted_average.py
+++ pagayapro/utils/weighted_average.py
@@ -1,13 +1,7 @@
 from typing import List
 
-import numpy as np
-
-
 def compute_weighted_average(x: List[float],
                              w: List[float])-> float:
-    if sum(w) == 0:
-        return np.NAN
-    else:
         return sum([x1 * w1 for x1, w1 in zip(x, w)]) / sum(w)
 
 def check_compute_weighted_average(x:List[float],
