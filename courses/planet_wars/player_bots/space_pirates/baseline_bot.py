d08bc152f577326f691399ba72ef3dae66fa1d7a
tal.ellinson <tal.ellinson@pagayapro.com>
2021-09-23 20:47:52 +0300

    created new branch and added most up to date baseline_bot.py

--- courses/planet_wars/player_bots/space_pirates/baseline_bot.py
+++ courses/planet_wars/player_bots/space_pirates/baseline_bot.py
@@ -6,90 +6,6 @@ from courses.planet_wars.tournament import get_map_by_id, run_and_view_battle, T
 
 import pandas as pd
 
-class Firstroundstrategy(Player):
-    EXTRA = 1
-    TURNS = 0
-    NAME="space_pirates"
-
-
-    def get_planets_to_attack(self, game: PlanetWars) -> List[Planet]:
-        """
-        :param game: PlanetWars object representing the map
-        :return: The planets we need to attack
-        """
-        return [p for p in game.planets if p.owner != PlanetWars.ME]
-
-    def ships_to_send_in_a_flee(self, source_planet: Planet, dest_planet: Planet) -> int:
-        future_enemy_size = dest_planet.num_ships + dest_planet.growth_rate*Planet.distance_between_planets(source_planet, dest_planet)
-        return future_enemy_size+Firstroundstrategy.EXTRA
-
-    def play_turn(self, game: PlanetWars) -> Iterable[Order]:
-        Firstroundstrategy.TURNS+=1
-        """
-        See player.play_turn documentation.
-        :param game: PlanetWars object representing the map - use it to fetch all the planets and flees in the map.
-        :return: List of orders to execute, each order sends ship from a planet I own to other planet.
-        """
-        # (1) If we currently have a fleet in flight, just do nothing.
-        if len(game.get_fleets_by_owner(owner=PlanetWars.ME)) >= 1:
-            return []
-
-        # (2) Find my strongest planet.
-        my_planets = game.get_planets_by_owner(owner=PlanetWars.ME)
-        if len(my_planets) == 0:
-            return []
-        my_strongest_planet = max(my_planets, key=lambda planet: planet.num_ships)
-
-        # (3) Find the weakest enemy or neutral planet.
-        planets_to_attack = self.get_planets_to_attack(game)
-        if len(planets_to_attack) == 0:
-            return []
-        planet_distances = {}
-        #distance_weight = 3
-        #strength_weight = 0.05
-        for i in planets_to_attack:
-            if i.num_ships < my_strongest_planet.num_ships:
-                ret_dist = Planet.distance_between_planets(my_strongest_planet,i)
-                score = ret_dist*ret_dist + i.num_ships
-                planet_distances[i.planet_id]=score
-        enemy_or_neutral_weakest_score_id = max(planet_distances.keys(), key=lambda planet: planet_distances[planet])
-        return [Order(
-            my_strongest_planet,
-            enemy_or_neutral_weakest_score_id,
-            self.ships_to_send_in_a_flee(my_strongest_planet, game.get_planet_by_id(enemy_or_neutral_weakest_score_id))
-        )]
-
-    def replay(self):
-# (1) If we currently have a fleet in flight, just do nothing.
-        if len(game.get_fleets_by_owner(owner=PlanetWars.ME)) >= 1:
-            return []
-
-        # (2) Find my strongest planet.
-        my_planets = game.get_planets_by_owner(owner=PlanetWars.ME)
-        if len(my_planets) == 0:
-            return []
-        my_strongest_planet = max(my_planets, key=lambda planet: planet.num_ships)
-
-        # (3) Find the weakest enemy or neutral planet.
-        planets_to_attack = self.get_planets_to_attack(game)
-        if len(planets_to_attack) == 0:
-            return []
-        planet_distances = {}
-        #distance_weight = 3
-        #strength_weight = 0.05
-        for i in planets_to_attack:
-            if i.num_ships < my_strongest_planet.num_ships:
-                ret_dist = Planet.distance_between_planets(my_strongest_planet,i)
-                score = ret_dist*ret_dist + i.num_ships
-                planet_distances[i.planet_id]=score
-        enemy_or_neutral_weakest_score_id = max(planet_distances.keys(), key=lambda planet: planet_distances[planet])
-        return [Order(
-            my_strongest_planet,
-            enemy_or_neutral_weakest_score_id,
-            self.ships_to_send_in_a_flee(my_strongest_planet, game.get_planet_by_id(enemy_or_neutral_weakest_score_id))
-        )]
-
-
 class AttackWeakestPlanetFromStrongestBot(Player):
     """
     Example of very simple bot - it send flee from its strongest planet to the weakest enemy/neutral planet
@@ -187,7 +103,7 @@ def view_bots_battle():
     Requirements: Java should be installed on your device.
     """
     map_str = get_random_map()
-    run_and_view_battle(AttackWeakestPlanetFromStrongestBot(), AttackEnemyWeakestPlanetFromStrongestBot(), map_str)
+    run_and_view_battle(Firstroundstrategy(), AttackEnemyWeakestPlanetFromStrongestBot(), map_str)
 
 
 def test_bot():
@@ -218,7 +134,154 @@ def test_bot():
     # To view battle number 4 uncomment the line below
     # tester.view_battle(4)
 
+class Firstroundstrategy(Player):
+    NAME="space_pirates"
+    #"accumulate_attack", "combine_attack", "accumulate"
+    STATE = "accumulate"
+    def get_planets_to_attack(self, game: PlanetWars) -> List[Planet]:
+        # if Firstroundstrategy.STATE == 'accumulate_attack':
+        if game.turns > 60:
+            return [p for p in game.planets if p.owner == PlanetWars.ENEMY]
+        return [p for p in game.planets if p.owner != PlanetWars.ME]
+
+    def ships_to_send_in_a_flee(self, source_planet: Planet, dest_planet: Planet) -> int:
+        if dest_planet.owner == 0:
+            return dest_planet.num_ships + 1
+        return dest_planet.num_ships + dest_planet.growth_rate*Planet.distance_between_planets(source_planet,dest_planet) + 1
+
+    # def combine_attack(self, game: PlanetWars) -> Iterable[Order]:
+    #     planets_to_attack = game.get_planets_by_owner(owner=PlanetWars.ENEMY)
+    #     my_planets = game.get_planets_by_owner(owner=PlanetWars.ME)
+    #     planet_to_attack = max(planets_to_attack, key=lambda planet: planet.num_ships)
+    #     if len(planets_to_attack) == 0:
+    #         return []
+    #     planet_scores = {}
+    #     for i in my_planets:
+    #         ret_dist = Planet.distance_between_planets(planet_to_attack, i)
+    #         if (i.growth_rate == 0) or (i.num_ships == 0):
+    #             score = ret_dist * ret_dist
+    #         else:
+    #             score = ret_dist * ret_dist / i.num_ships
+    #         planet_scores[i.planet_id] = score
+    #     if len(planet_scores) == 0:
+    #         return []
+    #     # enemy_or_neutral_weakest_score_id = min(planet_scores.keys(), key=lambda planet: planet_scores[planet])
+    #
+    #     scores_series = pd.Series(planet_scores, name='score')
+    #     scores_series.sort_values(inplace=True)
+    #     scores_df = scores_series.to_frame().reset_index()
+    #     # target_1 = min(planet_scores.keys(), key=lambda planet: planet_scores[planet])
+    #
+    #     counter = 0
+    #     current_sum = 0
+    #
+    #     for i in range(len(scores_series - 1)):
+    #         current_planet = game.get_planet_by_id(scores_df.loc[i, 'index'])
+    #
+    #         if (current_sum + current_planet.num_ships) < planet_to_attack.num_ships:
+    #             counter = counter + 1
+    #             current_sum += current_planet.num_ships
+    #     if counter == 0:
+    #         return []
+    #
+    #     order_list = []
+    #
+    #     for i in range(counter):
+    #         order_list.append(Order(
+    #             scores_df.loc[i, 'index'],  # planet id
+    #             planet_to_attack,
+    #             self.ships_to_send_in_a_flee(game.get_planet_by_id(scores_df.loc[i, 'index']), planet_to_attack)
+    #         ))
+    #
+    #     return order_list
+
+    def play_turn(self, game: PlanetWars) -> Iterable[Order]:
+        # (1) If we currently have a fleet in flight, just do nothing.
+        if len(game.get_fleets_by_owner(owner=PlanetWars.ME)) >= 1:
+            return []
+
+        # (2) Find my strongest planet.
+        my_planets = game.get_planets_by_owner(owner=PlanetWars.ME)
+        enemy_planets = game.get_planets_by_owner(owner=PlanetWars.ENEMY)
+        my_ships = game.total_ships_by_owner(owner=PlanetWars.ME)
+        enemy_ships = game.total_ships_by_owner(owner=PlanetWars.ENEMY)
+
+        if len(my_planets) == 0:
+            return []
+        my_strongest_planet = max(my_planets, key=lambda planet: planet.num_ships)
+        # (3) Find the weakest enemy or neutral planet.
+
+        # if (len(my_planets) < len(enemy_planets)) and (my_ships < enemy_ships):
+        #     Firstroundstrategy.STATE = "accumulate"
+        # else: #(len(my_planets) < len(enemy_planets)) and (my_ships > enemy_ships):
+        #     Firstroundstrategy.STATE = "accumulate_attack"
+        # # elif (len(my_planets) > len(enemy_planets)) and (my_ships < enemy_ships):
+        # #     Firstroundstrategy.STATE = "combine_attack"
+        #     return combine_attack(self, game)
+        # else:
+        #     Firstroundstrategy.STATE = "combine_attack"
+        #     return combine_attack(self, game)
+
+        planets_to_attack = self.get_planets_to_attack(game)
+        if len(planets_to_attack) == 0:
+            return []
+        planet_scores = {}
+        for i in planets_to_attack:
+            if i.num_ships < my_strongest_planet.num_ships:
+                ret_dist = Planet.distance_between_planets(my_strongest_planet,i)
+                if i.growth_rate == 0:
+                    score = ret_dist * ret_dist * i.num_ships
+                else:
+                    score = ret_dist*ret_dist*i.num_ships/i.growth_rate
+                planet_scores[i.planet_id]=score
+        if len(planet_scores)==0:
+            return []
+        # enemy_or_neutral_weakest_score_id = min(planet_scores.keys(), key=lambda planet: planet_scores[planet])
+
+        scores_series = pd.Series(planet_scores, name='score')
+        scores_series.sort_values(inplace=True)
+        scores_df = scores_series.to_frame().reset_index()
+        # target_1 = min(planet_scores.keys(), key=lambda planet: planet_scores[planet])
+
+        counter = 0
+        current_sum = 0
+
+        for i in range(len(scores_series-1)):
+            current_planet = game.get_planet_by_id(scores_df.loc[i,'index'])
+
+            if (current_sum + current_planet.num_ships) < my_strongest_planet.num_ships:
+                counter = counter + 1
+                current_sum += current_planet.num_ships
+        if counter == 0:
+            return []
+
+        order_list = []
+
+        for i in range(counter):
+                my_strongest_planet = max(my_planets, key=lambda planet: planet.num_ships)
+                order_list.append(Order(
+                my_strongest_planet,
+                scores_df.loc[i,'index'], #planet id
+                self.ships_to_send_in_a_flee(my_strongest_planet, game.get_planet_by_id(scores_df.loc[i,'index']))
+            ))
+
+        return order_list
+
+
+        #new below
+
+
+        # return [Order(
+        #     my_strongest_planet,
+        #     target_1,
+        #     self.ships_to_send_in_a_flee(my_strongest_planet, game.get_planet_by_id(enemy_or_neutral_weakest_score_id))
+        # ),Order(
+        #     my_strongest_planet,
+        #     enemy_or_neutral_weakest_score_id,
+        #     self.ships_to_send_in_a_flee(my_strongest_planet, game.get_planet_by_id(enemy_or_neutral_weakest_score_id))
+        # )]
+#end
 
 if __name__ == "__main__":
     test_bot()
-    view_bots_battle()
+    #view_bots_battle()
