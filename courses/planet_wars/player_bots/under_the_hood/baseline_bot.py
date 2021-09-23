d16bd5a0dccb588f94316f0d4bb2529d505cc059
Roei Zigelman <roei.zigelman@Pagayapro.com>
2021-09-23 15:36:35 +0300

    Removed imports

--- courses/planet_wars/player_bots/under_the_hood/baseline_bot.py
+++ courses/planet_wars/player_bots/under_the_hood/baseline_bot.py
@@ -2,8 +2,8 @@ import random
 from typing import Iterable, List
 
 from courses.planet_wars.planet_wars import Player, PlanetWars, Order, Planet
-from courses.planet_wars.player_bots.ender.EnderBot import EnderBot
-from courses.planet_wars.player_bots.kong_fu_pandas.baseline_bot import KongFuSyrianPandas
+#from courses.planet_wars.player_bots.ender.EnderBot import EnderBot
+#from courses.planet_wars.player_bots.kong_fu_pandas.baseline_bot import KongFuSyrianPandas
 from courses.planet_wars.tournament import get_map_by_id, run_and_view_battle, TestBot
 
 import pandas as pd
