5f88748ea1d95ff6e18beb7111ce9105b9661464
shira.turgeman <shiraturgeman@gmail.com>
2021-09-26 19:00:23 +0300

    Introduction to py.test

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -7,7 +7,7 @@ https://www.guru99.com/pytest-tutorial.html
 import random
 from typing import Union
 from typing import List
-
+import pandas as pd
 import pytest
 
 
@@ -73,6 +73,10 @@ def test_fibonacci_using_fixture(first_fibonacci_numbers):
 def please_test_me(string: str) -> str:
     return string + "!!!"
 
+def test_please_test_me():
+    assert please_test_me("shira") == "shira!!!"
+
+
 
 def times_7(number: Union[int, float]):
     return number * 7
@@ -85,6 +89,7 @@ def test_make_me_2_functions_one_use_fixture_and_one_use_parametrize():
     assert times_7(0) == 0
     assert times_7(-1) == -7
     # TODO add one interesting case I didn't check
+    assert times_7(-7) == -49
 
     random_generator = random.Random()
     for i in range(10):
@@ -93,12 +98,55 @@ def test_make_me_2_functions_one_use_fixture_and_one_use_parametrize():
         assert times_7(rnd_int) == sum([rnd_int for i in range(7)])
 
         # assert times_7(rnd_int) > rnd_int  # TODO Explain why this assert doest work
+        #because rnd_int can be negative
+
+@pytest.mark.parametrize("number, result", [(2,14), (4,28), (0,0), (-1,-7)])
+def test_make_me_2_use_parametrize(number, result):
+    assert times_7(number) == result
+
+@pytest.fixture
+def func_parameters():
+    return_list = []
+    random_generator = random.Random()
+    for i in range(10):
+        rnd_int = random_generator.randint(-1000, 1000)
+        return_list.append(rnd_int)
+    return return_list
+
+def test_make_me_2_functions_use_fixture(func_parameters):
+        for rnd_int in func_parameters:
+            # time_7(rnd_int) is like summing 7 items of rnd_int
+            assert times_7(rnd_int) == sum([rnd_int for i in range(7)])
 
 
 # TODO Add a function and at least 3 tests
+def divide_by_3(num):
+    return num/3
+
+def test_divide_by_3():
+    assert divide_by_3(3) == 1
+    assert divide_by_3(30) == 10
+    assert divide_by_3(90) == 30
+
 
 # TODO add a function that get data frame as an argument and return it after some preprocess/change
+def add_sum(df):
+    df['a'] = 0
+    return df
+
 # TODO test the function you wrote use assert_frame_equal and assert_series_equal
+def test_add_sum():
+    data = [{'a': 1, 'b': 2, 'c': 3},
+            {'a': 10, 'b': 20, 'c': 30}]
+    df = pd.DataFrame(data)
+
+    excepted_result = df.copy()
+    excepted_result['a'] = 0
+
+    pd.testing.assert_frame_equal(excepted_result, add_sum(df))
+    pd.testing.assert_frame_equal(excepted_result, add_sum(df))
+
+
 
 
 def compute_weighted_average(x: List[float], w: List[float]) -> float:
@@ -106,4 +154,6 @@ def compute_weighted_average(x: List[float], w: List[float]) -> float:
 
 
 def test_weighted_average_raise_zero_division_error():
-    pass  # TODO check that weighted_average raise zero division error when the sum of the weights is 0
+    # TODO check that weighted_average raise zero division error when the sum of the weights is 0
+    with pytest.raises(ZeroDivisionError):
+        assert compute_weighted_average([1, 1, 1, 1], [])
