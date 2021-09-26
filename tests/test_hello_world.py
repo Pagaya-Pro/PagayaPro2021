2e11980fbb84c73657fbc74b579c40c4afd9b076
Roei Zigelman <roei.zigelman@Pagayapro.com>
2021-09-26 18:52:36 +0300

    Completed TODOs

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -9,6 +9,7 @@ from typing import Union
 from typing import List
 
 import pytest
+import pandas as pd
 
 
 def test_simple_test():
@@ -74,8 +75,29 @@ def please_test_me(string: str) -> str:
     return string + "!!!"
 
 
+def test_please_test_me():
+    assert please_test_me("testing is great") == "testing is great!!!"
+
+
 def times_7(number: Union[int, float]):
     return number * 7
+
+
+@pytest.fixture
+def ten_randoms_neg_1k_to_1k():
+    random_generator = random.Random()
+    rnd_ints = [random_generator.randint(-1000, 1000) for i in range(10)]
+    return rnd_ints
+
+
+def test_times_7_using_fixture():
+    for rnd_int in ten_randoms_neg_1k_to_1k:
+        assert times_7(rnd_int) == sum(rnd_int for i in range(7))
+
+
+@pytest.mark.parametrize("number, number_multiplied_by_7", [(2, 14), (4, 28), (0, 0), (-1, -7), (0.5, 3.5)])
+def test_times_7_using_parametrize(number, number_multiplied_by_7):
+    assert times_7(number) == number_multiplied_by_7
 
 
 # TODO make_me_2_functions_one_use_fixture_and_one_use_parametrize
@@ -85,6 +107,7 @@ def test_make_me_2_functions_one_use_fixture_and_one_use_parametrize():
     assert times_7(0) == 0
     assert times_7(-1) == -7
     # TODO add one interesting case I didn't check
+    assert times_7(0.5) == 3.5
 
     random_generator = random.Random()
     for i in range(10):
@@ -93,12 +116,43 @@ def test_make_me_2_functions_one_use_fixture_and_one_use_parametrize():
         assert times_7(rnd_int) == sum([rnd_int for i in range(7)])
 
         # assert times_7(rnd_int) > rnd_int  # TODO Explain why this assert doest work
+        # this does not hold in case rnd_int is negative.
+        # also, function might return rnd_int+1 and still pass this assert
 
 
 # TODO Add a function and at least 3 tests
+def find_reciprocal(x: Union[int, float]):
+    return 1/x
+
+
+def test_find_reciprocal():
+    assert find_reciprocal(1) == 1
+    assert find_reciprocal(8) == 0.125
+    assert find_reciprocal(0.2) == 5
+
 
 # TODO add a function that get data frame as an argument and return it after some preprocess/change
+def mess_with_df(df):
+    first_col = df.columns[0]
+    df[first_col] = 1
+    return df
+
+
+@pytest.fixture
+def data_frame_example():
+    df = pd.DataFrame({"first": [1, 2, 3, 4, 5], "B": [5, 5, 5, 5, 5]})
+    return df
+
+
 # TODO test the function you wrote use assert_frame_equal and assert_series_equal
+def test_mess_with_df(data_frame_example):
+    expected_result = pd.DataFrame({"first": [1, 1, 1, 1, 1], "B": [5, 5, 5, 5, 5]})
+    pd.testing.assert_frame_equal(
+        mess_with_df(data_frame_example), expected_result
+    )
+    pd.testing.assert_series_equal(
+        mess_with_df(data_frame_example)["first"], expected_result["first"]
+    )
 
 
 def compute_weighted_average(x: List[float], w: List[float]) -> float:
@@ -106,4 +160,6 @@ def compute_weighted_average(x: List[float], w: List[float]) -> float:
 
 
 def test_weighted_average_raise_zero_division_error():
-    pass  # TODO check that weighted_average raise zero division error when the sum of the weights is 0
+    # TODO check that weighted_average raise zero division error when the sum of the weights is 0
+    with pytest.raises(ZeroDivisionError):
+        assert compute_weighted_average([50, 12, 3], [1, 0, -1])
