2dc88100e7bced9a5028bd1a76447441a4746799
Daniel Zaretsky <daniel.zaretsky@pagayapro.com>
2021-09-28 17:00:48 +0300

    testing

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -8,6 +8,8 @@ import random
 from typing import Union
 from typing import List
 
+import numpy as np
+import pandas as pd
 import pytest
 
 
@@ -74,6 +76,12 @@ def please_test_me(string: str) -> str:
     return string + "!!!"
 
 
+@pytest.mark.parametrize("word", [3, ""])
+def test_please_test_me(word):
+    assert isinstance(please_test_me(word), str), "Please use strings."
+    assert len(please_test_me(word)) == len(word) + 3
+
+
 def times_7(number: Union[int, float]):
     return number * 7
 
@@ -90,15 +98,102 @@ def test_make_me_2_functions_one_use_fixture_and_one_use_parametrize():
     for i in range(10):
         rnd_int = random_generator.randint(-1000, 1000)
         # time_7(rnd_int) is like summing 7 items of rnd_int
-        assert times_7(rnd_int) == sum([rnd_int for i in range(7)])
+        assert times_7(rnd_int) == sum([rnd_int for _ in range(7)])
 
         # assert times_7(rnd_int) > rnd_int  # TODO Explain why this assert doest work
+        # negative numbers / zero
+
+
+@pytest.mark.parametrize("number, times_seven", [(0, 0), (-1, -7), (2, 14), (4, 28), (np.Inf, np.inf)])
+def test_times_7_param(number, times_seven):
+    assert times_7(number) == times_seven
+
+
+def mult_by_sum(num, how_many):
+    """
+    This calculates multiplication by summation.
+    """
+    return sum([num for _ in range(how_many)])
+
+
+@pytest.fixture
+def rnd_int_list():
+    random_generator = random.Random()
+    rnd_int_list = [random_generator.randint(-1000, 1000) for _ in range(10)]
+    return rnd_int_list
+
+
+def test_times_7_fixture(rnd_int_list):
+    # using an implementation of multiplication by summation through a fixture.
+    for rnd_int in rnd_int_list:
+        assert times_7(rnd_int) == mult_by_sum(rnd_int, 7)
 
 
 # TODO Add a function and at least 3 tests
+def normalize(arr):
+    '''
+    Args:
+        arr: 1D array of numbers
+
+    Returns:
+        Normalized array
+    '''
+    std = np.std(arr)
+    if std == 0:
+        std = 1
+    return (arr - np.mean(arr)) / std
+
+
+@pytest.fixture(scope='module')
+def curr_arr():
+    return [1, 1, 1, 1]
+
+
+def test_normalize_empty(curr_arr):
+    assert len(curr_arr) != 0, "Please check that the array is not empty."
+
+
+def test_normalize_mean(curr_arr):
+    # Round in case it doesn't recognize it as 0, we have enough sign' digits.
+    assert round(np.mean(normalize(curr_arr)),10) == 0, "Please check infs"
+
+
+def test_normalize_std(curr_arr):
+    # Round in case it doesn't recognize it as 0, we have enough sign' digits.
+    assert round(np.std(normalize(curr_arr)),10) == 1, "Please use an array with more than 1 unique value."
+
 
 # TODO add a function that get data frame as an argument and return it after some preprocess/change
+def total_siblings_df(df):
+    '''
+    Args:
+        df: Dataframe
+    Returns: col1 without missing values, unless both cols have missing values, then return an empty df.
+    '''
+    df['siblings'] = df.brothers + df.sisters
+    return df
+
+
 # TODO test the function you wrote use assert_frame_equal and assert_series_equal
+@pytest.fixture
+def siblings_frame():
+    data = dict(
+        brothers=[2, 2, 3, 4, 5, 4, 3, 2, 1],
+        sisters=[0, 0, 0, 4, 5, 4, 3, 2, 1],
+    )
+    return pd.DataFrame.from_dict(data, orient='columns')
+
+
+def test_siblings(siblings_frame):
+    full_siblings_frame = total_siblings_df(siblings_frame)
+    # Test whole numbers
+    pd.testing.assert_frame_equal(full_siblings_frame, full_siblings_frame.astype(int)), \
+        'inputs should be whole numbers.'
+    # Test negative numbers / inf
+    bool_siblings = (full_siblings_frame['siblings'] >= 0)
+    bool_test = pd.Series(np.ones(len(bool_siblings,)).astype(bool))
+    pd.testing.assert_series_equal(bool_siblings, bool_test, check_names=False), \
+        'Total siblings containes negative or nan values'
 
 
 def compute_weighted_average(x: List[float], w: List[float]) -> float:
@@ -106,4 +201,5 @@ def compute_weighted_average(x: List[float], w: List[float]) -> float:
 
 
 def test_weighted_average_raise_zero_division_error():
-    pass  # TODO check that weighted_average raise zero division error when the sum of the weights is 0
+    with pytest.raises(ZeroDivisionError):
+        assert compute_weighted_average([1, 2, 1], [-2, 2, 0])
