08787192d0bbef47623d2c971c24399b115749b0
Daniel-Zaretsky <90546289+Daniel-Zaretsky@users.noreply.github.com>
2021-10-17 11:47:21 +0300

    Update clt.py
    Updated this clt using the other file

--- users/daniel.zaretsky/assignment1/clt.py
+++ users/daniel.zaretsky/assignment1/clt.py
@@ -1,30 +1,27 @@
-def means_of_samples(S, n):
+import random
+import matplotlib.pyplot as plt
+
+def means_of_samples(S: pd.Series, n: int) -> pd.Series:
     """
         The function takes a pandas series S and a natural number n and creates a series of means of samples (with replacement) of n entries from S.
-
         Parameters:
             S (pandas series): data to be sampled.
             n (natural number): number of samples per mean.
         Returns:
             Pandas series: A series of means of samples.
     """
-    import random
-    random.seed(99)
     k = 1000
-    means = []
-    return S.sample(n * k, replace = True).values.reshape(k, n).mean(axis = 1)
+    return S.sample(n * k, replace = True, random_state=99).values.reshape(k, n).mean(axis = 1)
 
-def hist_means(S, n):
+def hist_means(S: pd.Series, n: int):
     """
     The function takes a pandas series S and a natural number n and plots a histogram of the series of means, and also indicates the mean of this series.
-
     Parameters:
         S (pandas series): data to be sampled.
         n (natural number): number of samples per mean.
     """
-    sample = means_of_samples(S, n)
-    import matplotlib.pyplot as plt
-    plt.hist(sample)
+    means_series = means_of_samples(S, n)
+    plt.hist(means_series)
     plt.xlabel('Activation probability')
     plt.ylabel('Counts')
-    plt.title(f'Mean: {sample.mean():.3f}, Sample size: {n}')
+    plt.title(f'Mean: {means_series.mean():.3f}, Sample size: {n}')
