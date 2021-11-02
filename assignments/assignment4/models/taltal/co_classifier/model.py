5d256313d07b262dfba0316877ace49502c5ad6c
tal.ellinson <tal.ellinson@pagayapro.com>
2021-11-02 10:11:24 +0200

    updated files Tal and Tal

--- assignments/assignment4/models/taltal/co_classifier/model.py
+++ assignments/assignment4/models/taltal/co_classifier/model.py
@@ -1,27 +1,69 @@
-from co_classifier import preprocessor
-from co_classifier.preprocessor import Cleandata
+# from model import preprocessor
+from preprocessor import Cleandata
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
+from sklearn.base import BaseEstimator
+from sklearn.base import TransformerMixin
+# import co_classifier
 
-
-class Taltalim:
+class Taltalim (BaseEstimator, TransformerMixin):
     def fit(self, X, y):
-        self.pipe = Pipeline([('preprocessor', Cleandata()), ('scaler', StandardScaler()), ('logr', LogisticRegression(max_iter=1000))])
-        self.pipe.fit(X, y)
+        self.c = Cleandata()
+        X = self.c.fit_transform(X, y)
+        self.s = StandardScaler()
+        X = self.s.fit_transform(X, y)
+        self.l = LogisticRegression(max_iter=1000)
+        self.l.fit(X, y)
+
+        # self.pipe = Pipeline([('preprocessor', Cleandata()), ('scaler', StandardScaler()), ('logr', LogisticRegression(max_iter=1000))])
+        # self.pipe.fit(X, y)
 
     def predict(self, X, threshold=0.5):
         return self.predict_proba(X)[:, 1] > threshold
 
     def predict_proba(self, X):
-        return self.predict_proba(X)
+        X = self.c.transform(X)
+        X = self.s.transform(X)
+        return self.l.predict_proba(X)
+
 
 
 if __name__ == '__main__':
     import os
-    from pagayapro.paths.data_paths import ASSIGNMENT4_DATA
-    data = pd.read_parquet(os.path.join(ASSIGNMENT4_DATA, "prosper_data.parquet"))
+    # from pagayapro.paths.data_paths import ASSIGNMENT4_DATA
+    # data = pd.read_parquet(os.path.join(ASSIGNMENT4_DATA, "prosper_data.parquet"))
+    from sklearn.model_selection import train_test_split
+    from sklearn import metrics
+    data = pd.read_csv('data.csv')
+    # y = (~y.isna()).astype('int')
+    X, y = data.drop(['co_mob', 'co_amount'], axis=1), (~data.co_mob.isna()).astype('int')
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    # y_test = (~y_test.isna()).astype('int')
     cls = Taltalim()
+    cls.fit(X_train, y_train)
+    print(np.isnan(cls.predict_proba(X_test)).sum())
+    print(y_test)
+    # cls.predict(X_test).sum()
+    print(metrics.accuracy_score(y_test, cls.predict(X_test).astype('int')))
+    # X, y = Cleandata().fit_transform(data.drop('co_mob', axis=1), data.co_mob)
+    # print(type(X), type(y))
+    # s = StandardScaler()
+    # x__ = s.fit_transform(X, y)
+    # print(x__.shape)
+    # c = LogisticRegression(max_iter=1000)
+    # c.fit(x__, y)
+    print(X)
+
+
+
+
+
+
+
+
+
+
--- assignments/assignment4/models/taltal/co_classifier/preprocessor.py
+++ assignments/assignment4/models/taltal/co_classifier/preprocessor.py
@@ -1,32 +1,46 @@
 from sklearn import preprocessing
+from sklearn.base import RegressorMixin
+from sklearn.base import BaseEstimator
+from sklearn.base import TransformerMixin
 
-class Cleandata:
+
+class Cleandata(BaseEstimator, TransformerMixin):
 
     def __init__(self):
         pass
 
     def fit(self, X, y):
+        X = X.copy()
         self.le = preprocessing.LabelEncoder()
         self.le.fit(X.issue_date)
+        X['co'] = y
         self.occupation_to_co = X.groupby('occupation')['co'].mean()
+        X = X.drop('co', axis=1)
         self.to_drop = set()
+        df = X.corr().abs().stack()
+        # df[df < 1].sort_values(ascending=False).head(50)
         for tup in df[(df > .7) & (df < 1)].index:
-            if not set(tup) & to_drop:
+            if not set(tup) & self.to_drop:
                 self.to_drop.add(tup[0])
 
-    def transform(self, X):
+    def transform(self, X):#, y=None):
+        # if y is not None:
+        #     y = (~y.isna()).astype('int')
         X = X.copy()
         X = X.drop('borrower_city', axis=1)
         X['issue_date'] = self.le.transform(X.issue_date)
         X.occupation.fillna('Nan', inplace=True)
         X = X.join(self.occupation_to_co, on='occupation', rsuffix='_occupation') \
             .drop('occupation', axis=1).rename({'co_occupation': 'occupation_pr'})
-        X = X.drop(list(to_drop), axis=1)
+        X = X.drop('co', axis=1)
+        X = X.drop(list(self.to_drop), axis=1)
+        # if y is None:
+        #     return X
         return X
 
-    def fit_transform(self, X):
-        self.fit(X)
-        return self.transform(X)
+    def fit_transform(self, X, y):
+        self.fit(X, y)
+        return self.transform(X)#, y)
 
         # X = X.drop('borrower_city', axis=1)
         # self.le = preprocessing.LabelEncoder()
