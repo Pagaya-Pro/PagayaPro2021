5ef790b0dec759e1a46730327d2fc99a94516560
shais <shai.shechter@pagaya.com>
2021-12-06 07:53:57 +0200

    removed dropna(axis=1)

--- projects/data_exploration/preprocess.py
+++ projects/data_exploration/preprocess.py
@@ -1,12 +1,15 @@
-def preprocess_data(X,y=None, na_threshold = 0.6):
+import pandas as pd
+
+
+def preprocess_data(X,y=None):
     """
     preprocess for fitting and predicting on the model
     :param X: features
-    :param y: labels, optional for prediction
-    :param na_threshold: optional threshold for fropping nan columns
+    :param y: optional, labels for prediction
+    :param na_threshold: optional, threshold for dropping nan columns
     :return:
     """
-    df = X.assign(label=y) if y is not None else X
+    df = X.assign(label=y.values) if y is not None else X
     preprocessed_df = (
         df
         .assign(loan_amnt = pd.to_numeric(df.loan_amnt, errors="coerce"),
@@ -14,7 +17,6 @@ def preprocess_data(X,y=None, na_threshold = 0.6):
                )
         .dropna(subset=["int_rate",'label'])
         .select_dtypes(include="number")
-        .dropna(axis=1, how="all", thresh=int(na_threshold * df.shape[0]))
     )
     if y is None:
         return preprocessed_df
