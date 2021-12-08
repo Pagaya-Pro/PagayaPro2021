012ab84745d86a7e3b3de18a30811d04ec6154a0
tomerpagayapro <tomer.gal@pagayapro.com>
2021-12-08 15:22:37 +0200

    update utils files

--- projects/what_a_model_learns_on/utils.py
+++ projects/what_a_model_learns_on/utils.py
@@ -157,6 +157,9 @@ def set_information_date(df, info_year, info_month):
     df['prepaid_mob'] = prepaid_mobs
     return edit_prepaid_loans_payments(df)
 
+def calc_similarity(x, regular, initial):
+    return ((x - initial).sum()) ** 2 / (len(x) + regular)
+
 
 # ======================
 # Main Functions
@@ -192,7 +195,7 @@ def calc_irr(cashflows, info_date=None):
 
     return cashflows_payments.swifter.apply(npf.irr, axis=1).swifter.apply(lambda irr: ((irr + 1) ** 12 - 1) * 100,
                                                                            axis=1).fillna(-100)
-def calc_should(feature, y, regular=0):
+def calc_should(feature, y, regular=0, initial=0.5):
     """
     The function gets a certain binary feature column from a dataset and the label vector,
     and returns the "should" score of that feature, which is the gain of an XGBoost
@@ -202,9 +205,6 @@ def calc_should(feature, y, regular=0):
     of the similarity score.
     """
 
-    def calc_similarity(x, regular=0):
-        return ((x - 0.5).sum()) ** 2 / (len(x) + regular)
-
     assert (np.sort(feature.unique()) == [0, 1]).all(), "feature should only have 0s and 1s"
     assert len(feature) == len(y), "The dataframe and the label vector are not the same length"
 
@@ -212,9 +212,9 @@ def calc_should(feature, y, regular=0):
     zeros = y[feature == 0]
     ones = y[feature == 1]
 
-    zeros_similarity = calc_similarity(zeros, regular)
-    ones_similarity = calc_similarity(ones, regular)
-    root_similarity = calc_similarity(y, regular)
+    zeros_similarity = calc_similarity(zeros, regular, initial)
+    ones_similarity = calc_similarity(ones, regular, initial)
+    root_similarity = calc_similarity(y, regular, initial)
 
     gain = zeros_similarity + ones_similarity - root_similarity
 
