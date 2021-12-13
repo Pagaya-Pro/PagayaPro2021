dcedf29cafe181eb252546601f18e4ca46ed5cdf
tomerpagayapro <tomer.gal@pagayapro.com>
2021-12-13 12:52:36 +0200

    update should function

--- projects/what_a_model_learns_on/utils.py
+++ projects/what_a_model_learns_on/utils.py
@@ -211,27 +211,21 @@ def calc_irr(cashflows, info_date=None):
 
     return cashflows_payments.swifter.apply(npf.irr, axis=1).swifter.apply(lambda irr: ((irr + 1) ** 12 - 1) * 100,
                                                                            axis=1).fillna(-100)
-def calc_should(feature, y, regular=0, initial=0.5):
-    """
-    The function gets a certain binary feature column from a dataset and the label vector,
-    and returns the "should" score of that feature, which is the gain of an XGBoost
-    tree node that splits according to the feature.
-
-    regular is an optional parmeter that allows for adding regularization to the calculation
-    of the similarity score.
-    """
-
-    assert (np.sort(feature.unique()) == [0, 1]).all(), "feature should only have 0s and 1s"
-    assert len(feature) == len(y), "The dataframe and the label vector are not the same length"
 
 
-    zeros = y[feature == 0]
-    ones = y[feature == 1]
+def should(feature, y, regular=0):
+    scores = []
 
-    zeros_similarity = calc_similarity(zeros, regular, initial)
-    ones_similarity = calc_similarity(ones, regular, initial)
-    root_similarity = calc_similarity(y, regular, initial)
+    for initial in [np.max(y) + 1, np.min(y) - 1]:
+        zeros = y[feature == 0]
+        ones = y[feature == 1]
 
-    gain = zeros_similarity + ones_similarity - root_similarity
+        zeros_similarity = calc_similarity(zeros, regular, initial)
+        ones_similarity = calc_similarity(ones, regular, initial)
+        root_similarity = calc_similarity(y, regular, initial)
 
-    return gain
+        gain = zeros_similarity + ones_similarity - root_similarity
+
+        scores.append(max(gain / root_similarity, 0))
+
+    return np.sqrt(scores[0] * scores[1])
