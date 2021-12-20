6f3c79e9898d05b18ddfeb18fa36f743dbb6f323
omri.lerer <omri.lerer@pagayapro.com>
2021-12-20 18:18:15 +0200

    y

--- projects/what_a_model_learns_on/utils.py
+++ projects/what_a_model_learns_on/utils.py
@@ -467,17 +467,7 @@ def double_r(X, y, flag, model=None, seed=42, n_estimators=20, max_depth=6):
     if not model:
         if len(X) < 50000:
             print("Sample should be larger than 50,000")
-        zeros = X[flag == 0].index
-        ones = X[flag == 1].index
-        if len(zeros) != len(ones):
-            if len(zeros) < len(ones):
-                idx_to_keep = equalize(zeros, ones, seed)
-            else:
-                idx_to_keep = equalize(ones, zeros, seed)
-            idx_to_keep_bool = X.index.isin(idx_to_keep) == True
-            X = X[idx_to_keep_bool]
-            y = y[idx_to_keep_bool]
-            flag = flag[idx_to_keep_bool]
+
 
         if len(X) < 50000:
             print("Sample is too imbalanced. Either increase sample or reduce imbalance in flag.")
@@ -487,7 +477,17 @@ def double_r(X, y, flag, model=None, seed=42, n_estimators=20, max_depth=6):
             n_estimators=n_estimators,
             max_depth=max_depth)
         model.fit(X, y)
-
+    zeros = X[flag == 0].index
+    ones = X[flag == 1].index
+    if len(zeros) != len(ones):
+        if len(zeros) < len(ones):
+            idx_to_keep = equalize(zeros, ones, seed)
+        else:
+            idx_to_keep = equalize(ones, zeros, seed)
+        idx_to_keep_bool = X.index.isin(idx_to_keep) == True
+        X = X[idx_to_keep_bool]
+        y = y[idx_to_keep_bool]
+        flag = flag[idx_to_keep_bool]
     leaves = model.apply(X)
     return double_r_model(leaves, X, flag)
 
